6cc9c3f6960c107ca35ba81fb76104b29b9e8c91
lan496 <kohei19950508@gmail.com>
2022-09-08 20:33:42 +0900

    Remove redundant file

--- src/torch_m3gnet/data/MaterialGraphKey.py
+++ src/torch_m3gnet/data/MaterialGraphKey.py
@@ -1,1 +1,23 @@
-from ._keys import *  # noqa: F401, F403
+POS = "pos"
+ATOM_TYPES = "atom_types"
+NUM_TRIPLET_I = "num_triplet_i"
+EDGE_INDEX = "edge_index"
+EDGE_CELL_SHIFT = "edge_cell_shift"
+NUM_TRIPLET_IJ = "num_triplet_ij"
+TRIPLET_EDGE_INDEX = "triplet_edge_index"
+LATTICE = "lattice"
+NUM_NODES = "num_nodes"
+NUM_EDGES = "num_edges"
+NUM_TRIPLETS = "num_triplets"
+
+# Derived property
+EDGE_DISTANCES = "edge_distances"
+EDGE_WEIGHTS = "edge_weights"
+TRIPLET_ANGLES = "triplet_angles"
+
+# Features
+NODE_FEATURES = "x"
+EDGE_ATTR = "edge_attr"
+
+# BatchMaterialGraph
+BATCH = "batch"
